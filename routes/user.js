46f7d9b771d05a6c4315c485d8c7aa118864e9d0
MUHAMMAD BADHON <93506264+BADHON512@users.noreply.github.com>
2023-02-04 22:12:39 +0600

    Update user.js
    ol

--- routes/user.js
+++ routes/user.js
@@ -12,8 +12,12 @@ router.get("/googlelogin",passport.authenticate("google",{
 router.get(
   "/login",
   passport.authenticate("google", {
-    successRedirect: process.env.FRONTEND_URL
+      failureRedirect: '/login'
+    
   })
+   function(req, res) {
+    res.redirect(process.env.FRONTEND_URL);
+  }  
 );
 
 router.get('/me',isAthenticated,myProfile)
